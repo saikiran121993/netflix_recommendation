eccc737dc59cc105328f99b4dd686d7a2f65b2d7
Sai Kiran Rachakonda <saikiran.rachako@tigeranalytics.local>
2022-11-02 12:48:13 +0530

    'a'

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import plotly.graph_objects as go
 from streamlit_lottie import st_lottie
 import json
+import numpy  as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
@@ -11,28 +14,70 @@ def load_lottiefile(filepath: str):
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-netflix_data = pd.read_csv('netflix_titles.csv')
+df = pd.read_csv("Titles.csv")
+credits_df = pd.read_csv("Credits.csv")
+
+df["director"] = pd.merge(
+    df, credits_df[credits_df["role"] == "DIRECTOR"], on="id", how="left"
+)["name"].replace(np.nan, None)
+
+df["actors"] = pd.merge(
+    df,
+    pd.merge(df, credits_df[credits_df["role"] == "ACTOR"], on="id", how="left")
+    .groupby("id")["name"]
+    .apply(lambda x: x.tolist() if x is not np.nan else None),
+    on="id",
+    how="left",
+)["name"].apply(lambda x: ["" if i is np.nan else str(i) for i in x])
+
+df["actors"] = df["actors"].replace(np.nan, "")
+
+df["overview"] = (
+    (
+        df["title"].astype(str)
+        + " "
+        + df["description"].astype(str)
+        + " "
+        + df["genres"].apply(lambda x: " ".join(x))
+        + " "
+        + df["director"].astype(str)
+        + " "
+        + df["actors"].apply(lambda x: "" if x is [] else " ".join(x))
+    )
+    .str.lower()
+    .str.replace("\n", " ")
+    .str.replace("—", "")
+)
+
+count = CountVectorizer(stop_words="english", ngram_range=(1, 5))
+count_matrix = count.fit_transform(df["overview"])
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+indices = pd.Series(df.index, index=df["title"])
+count = CountVectorizer(stop_words="english", ngram_range=(1, 5))
+count_matrix = count.fit_transform(df["overview"])
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+indices = pd.Series(df.index, index=df["title"])
 
 
-tfidf = TfidfVectorizer(stop_words='english')
-netflix_data['description'] = netflix_data['description'].fillna('')
-tfidf_matrix = tfidf.fit_transform(netflix_data['description'])
-
-from sklearn.metrics.pairwise import linear_kernel
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-indices = pd.Series(netflix_data.index, index=netflix_data['title']).drop_duplicates()
-
-def get_recommendations(title, cosine_sim=cosine_sim):
+def get_recommendations(title, cosine_sim, top_k=5):
+    
     idx = indices[title]
+
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    movie_indices = [i[0] for i in sim_scores]
-    return netflix_data.iloc[movie_indices]
+    sim_scores = sim_scores[0:20]
+    movie_indices = [i[0] for i in sim_scores if i[0] != idx]
 
-#print(get_recommendations('Dark'))
-#tfidf_matrix.shape
+    return (
+        df.iloc[movie_indices]
+        .sort_values(["imdb_votes", "imdb_score"], ascending=False)[
+            ["title", "description", "genres", "imdb_score"]
+        ]
+        .reset_index(drop=True)
+        .head(top_k)
+    )
+
+
 def Table(df):
     fig=go.Figure(go.Table( columnorder = [1,2,3],
           columnwidth = [10,28],
@@ -47,12 +92,14 @@ def Table(df):
     fig.update_layout(height=500, title ={'text': "Top 10 Movie Recommendations", 'font': {'size': 22}},title_x=0.5
                      )
     return st.plotly_chart(fig,use_container_width=True)
-movie_list = netflix_data['title'].values
+
+movie_list = df['title'].values
 
 
 ####################################################################
 #streamlit
 ##################################################################
+
 
 st.header('Netflix Movie Recommendation System ')
 lottie_coding = load_lottiefile("m4.json")
@@ -69,7 +116,7 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names = get_recommendations(selected_movie)
+    recommended_movie_names = get_recommendations(selected_movie, cosine_sim=cosine_sim, top_k=10)    
     #list_of_recommended_movie = recommended_movie_names.to_list()
    # st.write(recommended_movie_names[['title', 'description']])
     Table(recommended_movie_names)
@@ -78,7 +125,8 @@ st.write('  '
          )
 st.write(' ')
 
-EDA = st.checkbox('Show Netflix Exploratory Data Analysis')
-if EDA :
-    st.write(
-        "check out this [link](https://www.kaggle.com/code/rushikeshdane20/in-depth-analysis-of-netflix-with-plotly)")
+
+# EDA = st.checkbox('Show Netflix Exploratory Data Analysis')
+# if EDA :
+#     st.write(
+#         "check out this [link](https://www.kaggle.com/code/rushikeshdane20/in-depth-analysis-of-netflix-with-plotly)")
